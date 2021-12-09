da36af2f20e1d3030319648087fc9c1f0e60351c
Aleksandar <aleksandarstaletovic99>
2021-12-10 00:11:06 +0100

    Ubacen index,style,slika

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Prijava</title>
+</head>
+
+<body>
+
+    <form action="#" method="post">
+        <div class="container">
+            <label class="username">Username</label>
+            <input type="text" name="username" class="form-control" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" name="password" class="form-control" required>
+            <button type="submit" name="submit">Sign In</button>
+        </div>
+    </form>
+
+
+</body>
+
+</html>
