5cd61161772cff6b1dbc382f7e62b55caa29f251
Aleksandar <aleksandarstaletovic99>
2021-12-27 12:12:46 +0100

    Dodata ajax funkcija za brisanje filmova

--- home.php
+++ home.php
@@ -65,20 +65,18 @@
                     </thead>
                     <tbody>
                         <?php
-                        $query = "SELECT filmID,naziv,budzet,opis,godina FROM film";
+                        $query = "SELECT filmId,naziv,budzet,opis,godina FROM film";
                         $filmovi = $conn->query($query);
                     while ($row = mysqli_fetch_array($filmovi)) {?>
-                            <tr>
-                            <div class="col-md-4"></div>
+                           <tr id="delete<?php echo $row['filmId'] ?>">
         
                                 <td><?php echo $row['naziv']  ?></td>
                                 <td><?php echo $row['budzet']  ?></td>
                                 <td><?php echo $row['opis']  ?></td>
                                 <td><?php echo $row['godina']  ?></td>
-                                <td><a href="kontroler/delete.php?id=<?php echo $row['filmID'] ?>">Obrisi</a></td>
-                                <a href="kontroler/edit.php?id=<?php echo $row['filmID'] ?>">Izmeni</a>
-
-                                <a href="kontroler/delete.php?id=<?php echo $row['filmID'] ?>">Obrisi</a>
+                            
+                                <td><button onclick="deleteAjax(<?php echo $row['filmId']; ?>)" class="btn btn-danger">Obrisi</button></td>
+                                <td><a href="kontroler/edit.php?id=<?php echo $row['filmId'] ?>">Izmeni</a></td>
 
                             </tr>
                             <?php } ?>
@@ -86,8 +84,27 @@
                 </table>
             </div>
         </div>
+        <script type="text/javascript">
+        function deleteAjax(filmId){
+                if(confirm('are You sure?')){
+                    $.ajax({
+                        type:'post',
+                        url:'kontroler/delete.php',
+                        data:{delete_filmId:filmId},
+                        success:function(data){
+                            $('#delete'+filmId).hide();
+                        }
+                        
+                    });
+                    
+                }
+                
+        }
+    </script>
     </main>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
                     </body>
--- kontroler/delete.php
+++ kontroler/delete.php
@@ -1,18 +1,9 @@
 <?php
 
-include("../broker.php");
+require "../broker.php";
+//require "../model/film.php";
 
+$filmId=$_POST['delete_filmId'];
+$query = mysqli_query($conn,"DELETE FROM film WHERE filmId='$filmId'");
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    $query = "DELETE FROM film WHERE filmID=$id";
-    $result = $conn->query($query);
-
-    if (!$result) {
-        die("Upit nije izvrsen!");
-    }
-    header("Location:../home.php");
-} else {
-    echo "GET NIJE IZVRSEN";
-}
+?>
